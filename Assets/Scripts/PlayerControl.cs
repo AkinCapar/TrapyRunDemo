b7821456f32699b26117c8cff84f950d2635d351
Akin Capar <akincapar2@gmail.com>
2021-08-21 22:06:14 +0300

    Player movement is done.

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -7,15 +7,33 @@ public class PlayerControl : MonoBehaviour
     private Rigidbody myBody;
     public float moveForce = 10f;
 
+    private DynamicJoystick joystick;
+
+
 
 
     private void Awake()
     {
-        
+        myBody = GetComponent<Rigidbody>();
+        joystick = GameObject.FindWithTag("Joystick").GetComponent<DynamicJoystick>();
     }
+
     // Update is called once per frame
     void Update()
     {
-        transform.position += transform.forward * moveForce * Time.deltaTime; 
+        transform.position += transform.forward * moveForce * Time.deltaTime;
+        transform.rotation = Quaternion.LookRotation(myBody.velocity);
+
+        if (joystick.Vertical < 0)
+        {
+            myBody.velocity = new Vector3(joystick.Horizontal, myBody.velocity.y,
+                myBody.velocity.z);
+        }
+
+        else
+        {
+            myBody.velocity = new Vector3(joystick.Horizontal, myBody.velocity.y,
+            joystick.Vertical);
+        }
     }
 }
