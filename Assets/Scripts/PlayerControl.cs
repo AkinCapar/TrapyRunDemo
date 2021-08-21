c5371bd944700109aa817b8144fb242ea6ba84f3
Akin Capar <akincapar2@gmail.com>
2021-08-21 16:56:29 +0300

    Very basic player movement is done.

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    private Rigidbody myBody;
+    public float moveForce = 10f;
+
+
+
+    private void Awake()
+    {
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += transform.forward * moveForce * Time.deltaTime; 
+    }
+}
