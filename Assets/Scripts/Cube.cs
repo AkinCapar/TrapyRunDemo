c6cfb06e84235c9d1c3b4b4782e45f11a41fc1e4
Akin Capar <akincapar2@gmail.com>
2021-08-22 17:42:37 +0300

    Cube color change and falling are done.

---
+++ Assets/Scripts/Cube.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+    private Rigidbody myBody;
+
+    public Color redColor;
+    MeshRenderer myRenderer;
+
+    public float colorTime = 0.1f;
+
+    private void Start()
+    {
+        myBody = GetComponent<Rigidbody>();
+        transform.position = new Vector3(transform.position.x, transform.position.y,
+            transform.position.z);
+
+        myRenderer = GetComponent<MeshRenderer>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (myBody.useGravity == true)
+        {
+
+            myRenderer.material.color = Color.Lerp(myRenderer.material.color,
+                redColor, colorTime);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "Player")
+        {
+            myBody.useGravity = true;
+        }
+    }
+}
